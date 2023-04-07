0659dc25914c343bdce68d713a223a08106abae2
Damjan Djermanovic <djermanovicdamjan@gmail.com>
2023-04-07 12:35:16 +0200

    Make reverseString function.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reverse = "";
+    for (let i = string.length; i > 0; i--){
+        reverse += string.charAt(i - 1);
+    }
+    return reverse;
 };
+
+console.log(reverseString("Goat"));
 
 // Do not edit below this line
 module.exports = reverseString;
