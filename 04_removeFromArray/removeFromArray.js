e2a3d51b943ae1f7e8f5a97b7c21834f78fe9c9d
Damjan Djermanovic <djermanovicdamjan@gmail.com>
2023-04-11 08:00:55 +0200

    Improve removeFromArray function.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,5 @@
 const removeFromArray = function(array, ...args) {
-
-    args.forEach(element => {
-        for (let item of array){
-            if (item === element) array.splice(array.indexOf(item), 1)
-        }
-    })
-
-    return array;
-
+    return array.filter(item => !args.includes(item));
 };
-
-console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3))
-
 // Do not edit below this line
 module.exports = removeFromArray;
